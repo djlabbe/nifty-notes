7fa39bc2c50b0151e24926db1af584450d6a1dc0
Douglas Labbe <dougjlabbe@gmail.com>
2019-09-05 21:33:43 -0700

    first lambda function

---
+++ scripts/createuser.sh
@@ -0,0 +1,5 @@
+aws cognito-idp sign-up \
+  --region us-west-2 \
+  --client-id 6c2qv247ah1ll5gbmmpvqda2j9 \
+  --username dlabbe@asu.edu \
+  --password Abcd1234!
---
+++ scripts/verifyuser.sh
@@ -0,0 +1,4 @@
+aws cognito-idp admin-confirm-sign-up \
+  --region us-west-2 \
+  --user-pool-id us-west-2_MGUCpYLHn \
+  --username dlabbe@asu.edu
