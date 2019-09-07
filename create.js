42da2514404499a05a762af6ef4358a913a1427a
Douglas Labbe <dougjlabbe@gmail.com>
2019-09-07 14:50:24 -0700

    formatting fixes

--- create.js
+++ create.js
@@ -1,6 +1,6 @@
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
-import {success, failure} from "./libs/response-lib";
+import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
@@ -20,6 +20,6 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    return failure({status: false});
+    return failure({ status: false });
   }
 }
